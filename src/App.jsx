e158f6abb03f00e9f3707b3b1dca2cc8f7bd7dc4
hamed <h.makled32@gmail.com>
2025-02-10 05:16:23 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./component/Home";
 import DetailsProduct from "./component/DetailsProduct";
+import Header from "./component/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
         <Routes>
           <Route path="/products" element={<Home />} />
           <Route path="/products/:id" element={<DetailsProduct />} />
